54b1ccfaf25b8753e70961d5f4cdebf2f27847bf
Yani Diaz <yani.m.diaz@gmail.com>
2021-06-13 19:22:19 -0300

    AGrego documentacion de clase Fermentador

--- TP3/CervezaArtesanal/Fermentador.cs
+++ TP3/CervezaArtesanal/Fermentador.cs
@@ -11,21 +11,38 @@ namespace CervezaArtesanal
         public float capacidadLitros;
         public ETipoCerveza tipoCerveza;
 
+        /// <summary>
+        /// COnstructor sin parametros. Le asigna un valores por defecto a los atributos
+        /// </summary>
         public Fermentador()
         {
             this.capacidadLitros = 50;
+            this.tipoCerveza = ETipoCerveza.IPA;
         }
 
+        /// <summary>
+        /// Sobrecarga del constructor, si recibe solo el tipo de cerveza lo asigna y le asigna el valor por defecto a la capacidad
+        /// </summary>
+        /// <param name="tipoCerveza"></param>
         public Fermentador(ETipoCerveza tipoCerveza) : this()
         {
+            this.tipoCerveza = tipoCerveza;
         }
 
-        public Fermentador(ETipoCerveza tipoCerveza, float capacidadLitros):this()
+        /// <summary>
+        ///  Sobrecarga del constructor, asigna valores recibidos por parametro a los atributos de clase
+        /// </summary>
+        /// <param name="tipoCerveza"></param>
+        /// <param name="capacidadLitros"></param>
+        public Fermentador(ETipoCerveza tipoCerveza, float capacidadLitros):this(tipoCerveza)
         {
-            this.tipoCerveza = tipoCerveza;
             this.capacidadLitros = capacidadLitros;
         }
 
+        /// <summary>
+        /// Propiedad de lectura y escritura. 
+        /// Si la capacidad que se le quiere asignar es mayor a la actual no lo permite
+        /// </summary>
         public float CapacidadLitros
         {
             get { return this.capacidadLitros;  }
@@ -39,6 +56,11 @@ namespace CervezaArtesanal
             }
             }
 
+        /// <summary>
+        /// Evalua si el fermentador soporta la capacidad que se le quiere asignar para almacenamiento
+        /// </summary>
+        /// <param name="litrosAAlmacenar"></param>
+        /// <returns>Si soporta la capacidad devuelve true, sino false</returns>
         public bool ValidarCapacidad(float litrosAAlmacenar)
         {
             bool tieneCapadidad = false;
